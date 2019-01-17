63c61b050222a4e7701043d064426c283f737e87
Mintyblot <46765712+Mintyblot@users.noreply.github.com>
2019-01-16 19:11:40 -0500

    Update bikeclass.java

--- bikeclass.java
+++ bikeclass.java
@@ -1,28 +1,35 @@
 class Bicycle {
 
-    int cadence = 0;
-    int speed = 0;
-    int gear = 1;
+  
+    int cadence = 0; // declaring a variable
+    int speed = 0; // declaring a variable
+    int gear = 1; // declaring a variable
 
-    void changeCadence(int newValue) {
-         cadence = newValue;
+    
+    void changeCadence(int newValue) { // This is the method for candence that sets the value of cadence
+         cadence = newValue; // initilizing a variable 
     }
 
-    void changeGear(int newValue) {
-         gear = newValue;
+  
+    void changeGear(int newValue) { // This is the method for gear that sets the value of gear
+         gear = newValue; // initilizing a variable 
     }
 
-    void speedUp(int increment) {
-         speed = speed + increment;   
+    
+    void speedUp(int increment) { // this is the method for calculating the speed
+         speed = speed + increment;   // calculaton of speed up variable  
     }
 
-    void applyBrakes(int decrement) {
-         speed = speed - decrement;
+   
+    void applyBrakes(int decrement) { // this is the method for calculating the reduction of speeds
+         speed = speed - decrement; // calculation of decrease of speed
     }
 
-    void printStates() {
+  
+    void printStates() { // this method is to print out results
          System.out.println("cadence:" +
              cadence + " speed:" + 
              speed + " gear:" + gear);
     }
+} 
 }
